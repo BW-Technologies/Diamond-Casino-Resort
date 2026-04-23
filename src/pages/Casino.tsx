a1abc4eddab12f9046433e836af309562ef9fc05
BlackWood <yvesyukine@gmail.com>
2026-04-23 15:42:51 +0200

    Update background image paths in Casino component

--- src/pages/Casino.tsx
+++ src/pages/Casino.tsx
@@ -9,7 +9,7 @@ export default function Casino() {
           label="L'EXPÉRIENCE DE L'ABONDANCE"
           title="LE CASINO"
           description={<p>Ici, vos rêves deviennent réalité, et la réalité est un rêve. Chaque caprice peut être satisfait, chaque fantasme assouvi – aucun contrôle de soi, aucune fenêtre, aucune horloge, et aucune sortie clairement indiquée. Bienvenue dans les standards du Diamond.</p>}
-          bgUrl="/5a7f62bca6ed6b2532659c32bf1feee17fcfa887.jpg"
+          bgUrl="./5a7f62bca6ed6b2532659c32bf1feee17fcfa887.jpg"
           textAlign="left"
         />
       </div>
@@ -19,7 +19,7 @@ export default function Casino() {
           label="L'EXPÉRIENCE DU PRESTIGE"
           title="JEUX DE TABLE"
           description="Ne vous y trompez pas. Roulette, blackjack, poker — au Diamond, ce ne sont pas de simples jeux. Chaque carte distribuée, chaque tour de roue, chaque réplique soignée, chaque frisson de la foule..."
-          bgUrl="/5015fae6149f15c920c01da3401f68033fb8b6f8.jpg"
+          bgUrl="./5015fae6149f15c920c01da3401f68033fb8b6f8.jpg"
           imagePosition="left"
         />
       </div>
